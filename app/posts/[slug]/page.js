eb2ab54682096ca6b7b887b2ae46f12d2a7841e7
BakedPotato101 <jackson.baker7562@gmail.com>
2025-05-27 16:51:09 -0500

    post formatting adjusted

--- app/posts/[slug]/page.js
+++ app/posts/[slug]/page.js
@@ -1,18 +1,6 @@
 import { getPost } from "@/utils/cosmic";
 import { Suspense } from "react";
 import { sanitize } from "isomorphic-dompurify";
-<<<<<<< HEAD
-
-export default async function Post({ params }) {
-  const post = await getPost(params.slug);
-  return (
-    <>
-      <div className="post text-2xl"
-        dangerouslySetInnerHTML={{
-          __html: sanitize(post.metadata.content) ?? "",
-        }}
-      ></div>
-=======
 import { Loading } from "@/components/Loading"
 
 export default async function Post({ params }) {
@@ -29,7 +17,6 @@ export default async function Post({ params }) {
           }}
         ></div>
       </Suspense>
->>>>>>> 832e6ee (post formatting adjusted)
     </>
   );
 }
