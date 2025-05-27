605effe64ebe2a95167079ffa06547e2549ccaf4
BakedPotato101 <jackson.baker7562@gmail.com>
2025-05-27 16:51:52 -0500

    adjusted post formatting

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,16 @@
 export default function Contact() {
-    return(
-        <div>
-            <h3>Contact</h3>
+    return (
+      <div>
+        <h3>Contact</h3>
+        <div className="text-xl">
+          <p>Feel free to reach out to me!</p>
+          <a className="text-accent underline">jackson.baker7562@gmail.com</a>
+          <br></br>
+          <a
+            className="text-accent underline"
+            href="https://www.linkedin.com/in/jackson-baker-956430237/"
+          >LinkedIn</a>
         </div>
-    )
+      </div>
+    );
 }
--- app/posts/[slug]/page.js
+++ app/posts/[slug]/page.js
@@ -1,6 +1,7 @@
 import { getPost } from "@/utils/cosmic";
 import { Suspense } from "react";
 import { sanitize } from "isomorphic-dompurify";
+<<<<<<< HEAD
 
 export default async function Post({ params }) {
   const post = await getPost(params.slug);
@@ -11,6 +12,24 @@ export default async function Post({ params }) {
           __html: sanitize(post.metadata.content) ?? "",
         }}
       ></div>
+=======
+import { Loading } from "@/components/Loading"
+
+export default async function Post({ params }) {
+  const { slug }= await params;
+  const post = await getPost(slug);
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        <h2>{post.title}</h2>
+        <div
+          className="flex flex-col post text-xl"
+          dangerouslySetInnerHTML={{
+            __html: sanitize(post.metadata.content) ?? "",
+          }}
+        ></div>
+      </Suspense>
+>>>>>>> 832e6ee (post formatting adjusted)
     </>
   );
 }
